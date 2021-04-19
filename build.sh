f768f2c923ddc17f38abd183e0eb7b5d457519d2
Pratik Chougule <planetpratik@users.noreply.github.com>
2021-04-19 12:00:13 +0530

    chore: Update build and run scripts to support Linux-ARM 64-Bit builds.

--- build.sh
+++ build.sh
@@ -12,10 +12,31 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     export VCPKG_FEATURE_FLAGS=versions
     # Save project root directory as we will need it to correctly provide vcpkg path in toolchain
     export HELLOWORLD_ROOT_DIRECTORY=$PWD
+    # Specifiy correct triplet you wish to use. If not specified, vcpkg will choose tirplet automatically based on compiler. 
+    export VCPKG_TARGET_TRIPLET=x64-osx
     cmake -B build/macOS -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$1 -DCMAKE_TOOLCHAIN_FILE=cmake/Toolchain/x86-64-macOS.cmake
     cd build/macOS
     # cmake --build . -- VERBOSE=1
     cmake --build .
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    if [ ! -d build/Linux/aarch64 ]; then
+    mkdir build/Linux/aarch64
+    fi
+    # As we are using vcpkg package manager, download and bootstrap it before using.
+    if [ ! -d vcpkg/vcpkg ]; then
+    git clone https://github.com/microsoft/vcpkg.git
+    ./vcpkg/bootstrap-vcpkg.sh
+    fi
+    # To enable versioning support in vcpkg, this flag must be set before.
+    export VCPKG_FEATURE_FLAGS=versions
+    # Save project root directory as we will need it to correctly provide vcpkg path in toolchain
+    export HELLOWORLD_ROOT_DIRECTORY=$PWD
+    # Specifiy correct triplet you wish to use. If not specified, vcpkg will choose tirplet automatically based on compiler. 
+    export VCPKG_TARGET_TRIPLET=arm64-linux
+    cmake -B build/Linux/aarch64 -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=$1 -DCMAKE_TOOLCHAIN_FILE=cmake/Toolchain/x64-ARM-Linux.cmake
+    cd build/Linux/aarch64
+    # cmake --build . -- VERBOSE=1
+    cmake --build .
 else
-    echo "macOS not detected. Run this script on macOS 64-Bit computer only."
+    echo "macOS ( 64-bit ) or Linux ( ARM-64 ) not detected. For windows platform, run build.bat file."
 fi
--- run.sh
+++ run.sh
@@ -2,6 +2,9 @@
 if [[ "$OSTYPE" == "darwin"* ]]; then
     cd build/macOS/source/App/
     ./app
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    cd build/Linux/aarch64/source/App/
+    ./app
 else
-    echo "macOS not detected. Run this script on macOS computer only."
+    echo "macOS ( 64-bit ) or Linux ( ARM-64 ) not detected. For windows platform, use run.bat file."
 fi
