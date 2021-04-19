fddea5b3cc2bd188fae78fb0e4e65d099be35bdf
Pratik Chougule <planetpratik@users.noreply.github.com>
2021-04-19 17:39:20 +0530

    refactor: Disable running remote executable until Headless rendering solution is found.

--- run.sh
+++ run.sh
@@ -4,7 +4,8 @@ if [[ "$OSTYPE" == "darwin"* ]]; then
     ./app
 elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
     cd build/Linux/aarch64/source/App/
-    ./app
+    # Disable running executable until Headless rendering is set up.
+    #./app
 else
     echo "macOS ( 64-bit ) or Linux ( ARM-64 ) not detected. For windows platform, use run.bat file."
 fi
