fa06aee75761ae7d614dd604fa71a77b334ca271
Pratik Chougule <planetpratik@users.noreply.github.com>
2022-02-05 11:02:49 +0530

    chore: Implement Run batch file to Run Windows application

---
+++ run.bat
@@ -0,0 +1,14 @@
+@echo off
+:: Check if user's windows os is 32-bit or 64-bit.
+reg Query "HKLM\Hardware\Description\System\CentralProcessor\0" | find /i "x86" > NUL && set CURRENT_OS=32BIT || set CURRENT_OS=64BIT
+:: If OS is 32-Bit, following code is executed.
+if %CURRENT_OS%==32BIT (
+    cd build/Windows/Win32/source/App/
+    app
+ ) else (
+    :: If OS is 64-Bit, following code is executed.
+    if %CURRENT_OS%==64BIT (
+    cd build/Windows/Win64/source/App/
+    app
+    ) 
+ )
