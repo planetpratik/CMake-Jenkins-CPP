778c496ebc8a6f5e43154b365b12f0c9c91beef5
Pratik Chougule <planetpratik@users.noreply.github.com>
2022-02-06 19:11:12 +0530

    chore: Update build.groovy script to determine Windows OS with Architecture

--- build.groovy
+++ build.groovy
@@ -1,15 +1,28 @@
 def findOS() {
+    String osname = System.getProperty('os.name');
+    def is64Bit = false;
     if (isUnix()) {
-        def osName = sh(script: 'uname', returnStdout: true)
-        if (osName.startsWith("Darwin")) {
+        def osNameByScript = sh(script: 'uname', returnStdout: true)
+        // Currently we aren't using below boolean for Unix like OS builds.
+        // But if needed, modify below if-else loop to return appropriate strings and modify Jenkinsfile to generate builds for those platforms.
+        is64Bit = (System.getProperty("os.arch").indexOf("64") != -1);
+        if (osNameByScript.startsWith("Darwin")) {
             return "macOS"
         }
         else {
             return "Linux"
         }
     }
-    else {
-        return "Windows"
+    else if (osname.contains("Windows")) {
+        if(System.getenv("ProgramFiles(x86)") != null) {
+            is64Bit = true;
+            return "Windows64"
+        }
+        else {
+            return "Windows32"
+        }
+    } else {
+        throw new Exception("Unsupported OS: ${osname}");
     }
 }
 return this
